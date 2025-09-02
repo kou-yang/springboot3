bdbcdb139191a9f62f65d3d01cf37e195c81298b
kouyang <kouyang0313@163.com>
2025-09-02 11:57:38 +0800

    build(server): 集成 MyBatis-Plus 分页插件

---
+++ server/src/main/java/com/conggua/springboot3/server/config/MyBatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.conggua.springboot3.server.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: kouyang
+ * @description:
+ * @date: 2025-08-19 14:41
+ */
+@Configuration
+@MapperScan("com.conggua.springboot3.server.mapper")
+public class MyBatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
